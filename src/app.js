9632d57313542a94dafbde2f26414d94d2ac47de
sahan <sahan.lamahewa@gmail.com>
2018-09-30 00:08:03 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("aaa");
+const Peer = require('simple-peer');
+
+const $ = require('jquery');
+const p = new Peer({initiator: location.hash === '#1', trickle:false});
+
+p.on('signal', token => {
+  $('#txtMySignal').val(JSON.stringify(token));
+});
+
+p.on('connect', () => {
+  console.log('connected');
+  setInterval(function(){
+    p.send(Math.random());
+  },2000)
+});
+
+p.on('data', data => console.log('NHAN DU LIEU:'+ data));
+
+$('#btnConnect').click(() => {
+  const friendSignal = JSON.parse($('#txtFriendSignal').val());
+  p.signal(friendSignal);
+})
